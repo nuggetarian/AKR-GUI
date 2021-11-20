c588156f351981bd854e9813f3b2012b4b07e1bf
nuggetarian <michal.zernovic@gmail.com>
2021-11-20 01:42:34 +0100

    logger

--- testgui.py
+++ testgui.py
@@ -1,9 +1,10 @@
-import hashlib, sqlite3
+import sqlite3
 from tkinter import *
 from tkinter import ttk
 from pwdatabase import PwDatabase
 from sqliteoperations import Database
 import os
+import logging
 from PIL import ImageTk, Image
 
 absolutepath = os.path.abspath(__file__)
@@ -11,7 +12,15 @@ fileDirectory = os.path.dirname(absolutepath)
 
 master = Tk()
 master.title("Password Manager")
-master.iconbitmap(fileDirectory + "\\lock.ico")
+master.iconbitmap(fileDirectory + "\\pictures\\lock.ico")
+
+# LOGGER
+logging.basicConfig(filename="logfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logging.getLogger('PIL').setLevel(logging.WARNING)
 
 #NIEČO SPRAVIŤ BIELE = xxxx.configure(background="white")
 
@@ -21,7 +30,7 @@ def firstScreen():
   for widget in master.winfo_children():
     widget.destroy()
 
-  fekt_img = Image.open('fekt.png')
+  fekt_img = Image.open(fileDirectory + '\\pictures\\fekt.png')
   fekt_img = fekt_img.resize((342,111), Image.ANTIALIAS)
   fekt_img = ImageTk.PhotoImage(fekt_img)
   fekt_label = ttk.Label(image=fekt_img)
@@ -29,24 +38,17 @@ def firstScreen():
   fekt_label.config(anchor=CENTER, background="white")
   fekt_label.pack()
 
-
-  """optionLbl = Label(master, text="Zvoľ možnosť:", font="Helvetica 15 bold")
-  optionLbl.config(anchor=CENTER, background="white")
-  optionLbl.pack(pady=5)"""
-
   global logInImage
-  logInImage = PhotoImage(file='login.png')
+  logInImage = PhotoImage(file=fileDirectory + '\\pictures\\login.png')
   
   logInBtn = Button(master, image=logInImage, command=logInScreen, borderwidth=0)
-  #logInBtn = Button(master, text="Log in", font="Helvetica", command=logInScreen)
   logInBtn.config(background="white")
   logInBtn.pack(pady=5)
   
   global signUpImage
-  signUpImage = PhotoImage(file='signup.png')
+  signUpImage = PhotoImage(file=fileDirectory + '\\pictures\\signup.png')
   signUpBtn = Button(master, image=signUpImage, command=signUpScreen, borderwidth=0)
   signUpBtn.configure(background="white")  
-  #signUpBtn = Button(master, text="Sign up", font="Helvetica", command=signUpScreen)
   signUpBtn.pack(pady=5)
 
 
@@ -56,6 +58,7 @@ def firstScreen():
   #treeViewBtn.pack(pady=5)
 
 def signUpScreen():
+  logger.info("Sign up chosen")
   for widget in master.winfo_children():
     widget.destroy()
   master.geometry("500x500")
@@ -83,6 +86,7 @@ def signUpScreen():
     firstScreen()
     #TUTO NEJAKE IFKO ČI SA PODARILO
     success = Label(master, text="Registrácia prebehla úspešne.", font="Helvetica", background="white")
+    logger.info("Sign up successful")
     success.config(anchor=CENTER)
     success.pack(pady = 5)
 
@@ -131,6 +135,7 @@ def treeView():
     my_tree.pack(pady=20)
 
 def logInScreen():
+  logger.info("Log in chosen")
   pwdb = PwDatabase()
   pwdb.createTable()
 
@@ -157,14 +162,13 @@ def logInScreen():
     password = passwordEntry.get()
     db = Database(mail, password)
     if (db.comparePasswords() == True):
+      logger.info("Log in successful as " + mail)
       success = Label(master, text="Spravne heslo", font="Helvetica")
       success.config(anchor=CENTER)
       success.pack()
     else:
+      logger.info("Log in unsuccessful as " + mail)
       popUp()
-      """failure = Label(master, text="Nespravne heslo", font="Helvetica")
-      failure.config(anchor=CENTER)
-      failure.pack()"""
 
   submitButton = Button(master, text="Submit", font="Helvetica", command=comparePasswords).pack(pady=5)
   backButton = Button(master, text="Back", font="Helvetica", command=firstScreen).pack(pady=5)
@@ -175,7 +179,7 @@ def popUp():
   popUpWindow.title("Warning")
   popUpWindow.resizable(False, False)
   popUpWindow.config(background="white")
-  popUpWindow.iconbitmap(fileDirectory + "\\warning.ico")
+  popUpWindow.iconbitmap(fileDirectory + "\\pictures\\warning.ico")
   warningLbl = Label(popUpWindow, text="NESPRÁVNE HESLO", font="Helvetica 10")
   warningLbl.config(anchor=CENTER, background="white")
   warningLbl.pack(pady=5)
